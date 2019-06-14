c7224169b1d10c37cf861ac33b70626a00a00b54
shadeying <shadeying@gmail.com>
2019-06-14 21:04:24 +0000

    Form for rating isbetween 1 to 5

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,14 @@
 class ReviewsController < ApplicationController
 
   def create
+
     @review = Review.new(
       description: review_params['description'],
       rating: review_params['rating'],
       product_id: params['product_id'],
       user_id: current_user.id
     )
+
     @review.save
 
     redirect_to products_path
