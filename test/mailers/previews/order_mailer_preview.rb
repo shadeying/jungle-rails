608e0502a8e3febf79d4b536fdc9218038144946
shadeying <shadeying@gmail.com>
2019-06-14 22:34:40 +0000

    Order email preview test fixed

--- test/mailers/previews/order_mailer_preview.rb
+++ test/mailers/previews/order_mailer_preview.rb
@@ -1,7 +1,7 @@
 class OrderMailerPreview < ActionMailer::Preview
 
   def order_receipt
-    OrderMailer.order_receipt(Order.first, Order.first.line_items)
+    OrderMailer.order_receipt(Order.first)
   end
 
 end
