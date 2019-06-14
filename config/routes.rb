3a6acb58135063215b925db324b87f86bf634cac
shadeying <shadeying@gmail.com>
2019-06-14 22:13:14 +0000

    Base controller added to AdminController

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
-    resources :categories, only: [:index, :create, :show, :new]
+    resources :categories, only: [:index, :create, :new]
   end
 
   get '/login' => 'sessions#new'
