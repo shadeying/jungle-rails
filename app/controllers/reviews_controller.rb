25b0e2679c4bfb27d558ef8e22df59251c572e30
shadeying <shadeying@gmail.com>
2019-06-14 21:28:47 +0000

    Logged in user can delete their own reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -14,6 +14,13 @@ class ReviewsController < ApplicationController
     @review.save
 
     redirect_to products_path
+
+  end
+
+  def destroy
+    @review = Review.find params[:id]
+    @review.destroy
+    redirect_to products_path, notice: 'Review deleted!'
   end
 
   private
