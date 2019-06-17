8ae4e05c2ae4ec5dbf39b3a0fc30df6442b6dbc2
shadeying <shadeying@gmail.com>
2019-06-17 19:03:24 +0000

    Test for inclusion of error message is added

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -22,21 +22,25 @@ RSpec.describe Product, type: :model do
     it 'is not valid without a name' do
       @product.name = nil
       expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Name can't be blank")
     end
 
     it 'is not valid without a price' do
-      @product.price = nil
+      @product.price_cents = nil
       expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Price can't be blank")
     end
 
     it 'is not valid without a quantity' do
       @product.quantity = nil
       expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
     end
 
     it 'is not valid without a category' do
       @product.category = nil
       expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Category can't be blank")
     end
   end
 
